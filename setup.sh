8a06da5c5f29678c40bc16c98f732e47b3b0b49e
chuksmomoh101231 <97413788+chuksmomoh101231@users.noreply.github.com>
2022-07-06 16:26:25 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit/echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+FROM python:3.9.12 
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirements.txt
+EXPOSE 80
+RUN mkdir ~/.streamlit
+RUN cp config.toml ~/.streamlit/config.toml
+RUN cp credentials.toml ~/.streamlit/credentials.toml
+WORKDIR /app
+ENTRYPOINT ["streamlit", "run"]
+CMD ["app002"]
